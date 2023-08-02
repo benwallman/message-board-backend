414ce529f391615d60d9683b2f5cec3e6037c128
Ben Wallman <benwallman@hotmail.com>
2023-08-02 19:27:36 +0100

    Completed all the methods I need to create the endpoint. Made the async, to be a bit more like when the DB is here, but async is ofc not needed here. Added a _clearMessages function, purely for testing. Not the best, but in the interests of time felt like a good shout

--- handlers/message.test.ts
+++ handlers/message.test.ts
@@ -1,8 +1,51 @@
-import { getAllMessages } from './message'
+import { v4 as uuid } from 'uuid'
+
+import {
+  getAllMessages,
+  createMessage,
+  getMessage,
+  deleteMessage,
+  _clearMessages,
+} from './message'
+
+beforeEach(() => {
+  _clearMessages()
+})
 
 describe('getAllMessages', () => {
   it('returns all messages', async () => {
     const messages = await getAllMessages()
     expect(messages).toEqual([])
+  })
+})
+
+describe('createMessage', () => {
+  it('creates a message', async () => {
+    const initialMessages = await getAllMessages()
+    expect(initialMessages.length).toEqual(0)
+
+    await createMessage('hello', uuid())
+    const messages = await getAllMessages()
+    expect(messages.length).toEqual(1)
+  })
+})
+
+describe('getMessage', () => {
+  it('gets a message by the ID', async () => {
+    const messageId = await createMessage('Hello World', uuid())
+    const message = await getMessage(messageId)
+    expect(message.text).toEqual('Hello World')
+  })
+})
+
+describe('deleteMessage', () => {
+  it('deletes a message by the ID', async () => {
+    const messageId = await createMessage('Hello World', uuid())
+    const initialMessages = await getAllMessages()
+    expect(initialMessages.length).toEqual(1)
+
+    await deleteMessage(messageId)
+    const messages = await getAllMessages()
+    expect(messages.length).toEqual(0)
   })
 })
--- handlers/message.ts
+++ handlers/message.ts
@@ -1,3 +1,5 @@
+import { v4 as uuid } from 'uuid'
+
 interface Message {
   id: string;
   text: string;
@@ -7,3 +9,38 @@ interface Message {
 let messages: Message[] = []
 
 export const getAllMessages = async () => messages
+
+// N.B. For unit testing only !!!
+export const _clearMessages = async () => {
+  await updateMessages([])
+}
+
+const updateMessages = async (newMessages: Message[]) => {
+  messages = newMessages
+}
+
+export const createMessage = async (text: string, user: string) => {
+  const id = uuid()
+  const message = { id, text, user }
+  const newMessages = [...messages, message]
+  updateMessages(newMessages)
+  return id
+}
+
+export const getMessage = async (id: string) => {
+  const message = messages.find(message => message.id === id)
+  if (!message) throw new Error('Message not found')
+  return message
+}
+
+export const updateMessage = async (id: string, text: string) => {
+  const message = await getMessage(id)
+  const newMessage = { ...message, text }
+  const newMessages = [...messages].map(message => message.id === id ? newMessage : message)
+  updateMessages(newMessages)
+}
+
+export const deleteMessage = async (id: string) => {
+  const refinedMessages = [...messages].filter((message, index) => message.id !== id)
+  updateMessages(refinedMessages)
+}
